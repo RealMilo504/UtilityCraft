cec0d98a4d9a2ea5c209b6883b8e52c50263e9a7
Dorios Studios <chavez100577@gmail.com>
2025-09-27 13:06:35 -0600

    Fixed Durability.js Issue

--- BP/scripts/anew/items/durability.js
+++ BP/scripts/anew/items/durability.js
@@ -2,6 +2,7 @@ import { world, ItemStack } from '@minecraft/server'
 
 world.afterEvents.playerBreakBlock.subscribe(({ itemStackAfterBreak, player }) => {
     if (!itemStackAfterBreak.typeId.startsWith('utilitycraft:')) return
+    if (!itemStackAfterBreak.hasComponent('durability')) return
     if (itemStackAfterBreak.durability.damage(1, 1)) {
         player.setEquipment("Mainhand", itemStackAfterBreak)
     } else {
@@ -15,6 +16,8 @@ world.afterEvents.entityHitEntity.subscribe(({ damagingEntity }) => {
     const player = damagingEntity
     /** @type {ItemStack} */
     const itemStack = player.getEquipment("Mainhand")
+    if (!itemStack.typeId.startsWith('utilitycraft:')) return
+    if (!itemStack.hasComponent('durability')) return
     if (itemStack.durability.damage(1, 1)) {
         player.setEquipment("Mainhand", itemStack)
     } else {
