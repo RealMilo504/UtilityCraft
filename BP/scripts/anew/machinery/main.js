6c8cf5e87087b7196b0be0c09931e0cddfcecdda
Milo504 <92042797+RealMilo504@users.noreply.github.com>
2025-10-09 20:19:52 -0600

    Refactor digitizer machine to new scripting API

--- BP/scripts/anew/machinery/main.js
+++ BP/scripts/anew/machinery/main.js
@@ -5,6 +5,7 @@ import './machines/double_machine.js'
 import './machines/block_breaker.js'
 import './machines/block_placer.js'
 import './machines/induction_anvil.js'
+import './machines/digitizer.js'
 import './managers.js'
 
 import './upgradeable.js'
